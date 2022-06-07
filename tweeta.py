99b3d3ac86804c837ea88260811da65e93eaf376
Vishal Choudhary <officialbulu@gmail.com>
2022-06-07 17:33:05 +0530

    Update tweeta.py

--- tweeta.py
+++ tweeta.py
@@ -2,10 +2,10 @@ from textblob import TextBlob
 import tweepy
 
 
-con_key = 'wrLnUwMx5Y14xnRTjaIUx9PY9'
-con_secrect = 'gA4dIGiDtBSy8GVehmG3ryGttH3GLzDLSnif5Lw0PxbmCq6h2S'
-acc_tok = '1145775012055519233-83pruLeKDnbs9v2wdaDAaRHDsDrxBQ'
-acc_tok_sec = '9q5MUy9abwGvbvT0Q9c69tNU3p28XBtmt5LkVKgHEKOjD'
+con_key = ''
+con_secrect = ''
+acc_tok = ''
+acc_tok_sec = ''
 
 auth = tweepy.OAuthHandler(con_key,con_secrect)
 
